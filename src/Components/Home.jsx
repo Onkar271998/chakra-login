8ec4fdbba5d2f8354f25e30662a3b63e2d728b8c
Onkar271998 <onkar271998@gmail.com>
2022-08-22 00:29:38 +0530

    up

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,27 @@
+import { Button, Input } from "@chakra-ui/react";
+
 function Home(){
     return(
         <>
-        jdbsjkvbsdjkbv
+        <div>
+
+          <h1 style={{fontSize:"62px" ,fontWeight:"bold"}}>Find The Right</h1>
+          <h1 style={{fontSize:"62px"}}>Four-Day Week job</h1>
+
+
+          <Input placeholder="Type job title or keyword" width="50%" marginBottom="10px" marginTop="5%"></Input> <br/>
+        
+
+          <Button  backgroundColor="#6369d2" color="white">Remote</Button>
+          <Button  marginLeft="10px" backgroundColor="#6369d2" color="white">Tech</Button>
+          <Button marginLeft="10px" backgroundColor="#6369d2" color="white">Marketing</Button>
+
+        
+          
+
+
+
+        </div>
         </>
     )
 }
